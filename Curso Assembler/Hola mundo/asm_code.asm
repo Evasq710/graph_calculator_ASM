d425bdffec835edd060d3437997eff22355f78c3
Evasq710 <evasq710@hotmail.com>
2022-12-20 22:21:43 -0600

    Hello world!

---
+++ Curso Assembler/Hola mundo/asm_code.asm
@@ -0,0 +1,21 @@
+;db -> define 1 byte:	8 bits
+;dw -> define 2 bytes:	16 bits
+;dd -> define 4 bytes:	32 bits
+;dq -> define 8 bytes:	64 bits
+
+section .data
+msg db "Hola mundo!",0x0a, 0x0d
+len equ $-msg	;Busca al centinela ($), para saber cuánto espacio ocupa 'msg', y lo guarda en la etiqueta 'len'
+
+section .text
+	global _start
+_start:
+mov eax, 4 ;indicando que se quiere escribir
+mov ebx, 1 ;indicando que se quiere escribir en pantalla
+mov ecx, msg ;indicando qué se quiere escribir
+mov edx, len ;indicar cuántos bytes se van a imprimir
+int 80h	;iniciar la interrupción
+
+;finalizando con proceso exitoso
+mov eax, 1
+int 0x80
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Assembler_201900131
+
+## Especificaciones técnicas
+
+- Procesador x86
+- Sistema operativo Linux
+- Ensamblador NASM
+- Sintaxis Intel
+
+Comando Linux para generar código objeto:
+
+```
+nasm -f elf FILE.asm
+
+```
+
+Comando Linux para generar ejecutable:
+
+```
+ld -m elf_i386 -o NAME_EXEC_FILE OBJECT_FILE.o
+
+```
+
