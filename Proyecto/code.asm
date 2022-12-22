583a68b1818f8265cce0299e8da917e33418d1ba
Evasq710 <evasq710@hotmail.com>
2022-12-22 04:00:27 -0600

    Macros impresión de números & Impresión de función

--- Proyecto/code.asm
+++ Proyecto/code.asm
@@ -1,4 +1,4 @@
-;; MACROS DE LECTURA Y ESCRITURA EN PANTALLA
+;; MACRO ESCRITURA EN PANTALLA
 %macro print 2
 mov eax, 4
 mov ebx, 1
@@ -7,6 +7,78 @@ mov edx, %2
 int 0x80
 %endmacro
 
+;; MACRO IMPRIMIR NUM DE 2 DIGITOS
+%macro printTwoDigits 1
+mov bl, 10 	; BL: Fuente de 8 bits
+mov al, %1	; AL: Resultado
+mov ah, 0	; AH: Residuo
+
+div bl ;Dividiendo AL / BL
+mov [residuo], ah
+
+add al, 48 ; ascii
+mov [cociente], al
+print cociente, 1
+
+mov ah, [residuo]
+add ah, 48 ; ascii
+mov [residuo], ah
+print residuo, 1
+%endmacro
+
+;; MACRO IMPRIMIR NUM DE 3 DIGITOS
+%macro printThreeDigits 1
+mov bl, 100	; BL: Fuente de 8 bits
+mov al, %1	; AL: Resultado
+mov ah, 0	; AH: Residuo
+
+div bl ;Dividiendo AL / BL
+mov [residuo], ah
+
+add al, 48 ; ascii
+mov [cociente], al
+print cociente, 1
+
+printTwoDigits [residuo]
+%endmacro
+
+;; MACRO IMPRIMIR NÚMERO
+%macro printNumber 1
+mov cl, %1
+
+mov bl, 100	; BL: Fuente de 8 bits
+mov al, cl	; AL: Resultado
+mov ah, 0	; AH: Residuo
+
+div bl 		;Dividiendo AL / BL
+cmp al, 0
+jne %%three	;Si el resultado no es cero, 3 digitos
+
+mov bl, 10
+mov al, cl	; AL: Resultado
+mov ah, 0	; AH: Residuo
+
+div bl	;Dividiendo AL / BL
+cmp al, 0
+jne %%two	;Si el resultado no es cero, 2 digitos
+
+jmp %%one	;Un digito
+
+%%three:
+printThreeDigits cl
+jmp %%exit_macro
+%%two:
+printTwoDigits cl
+jmp %%exit_macro
+%%one:
+add cl, 48	; ascii
+mov [byte_aux], cl
+print byte_aux, 1
+%%exit_macro:
+
+%endmacro
+
+;; MACRO LECTURA DE TECLADO
 %macro read 2
 mov eax, 3
 mov ebx, 0
@@ -15,16 +87,49 @@ mov edx, %2
 int 0x80
 %endmacro
 
-;; DEFINIENDO TEXTOS DE MENÚS
+
 section .data
-	text_menu	db	"========== CALCULADORA ARQUI 1 - Elias Vasquez ==========", 0xA, 0xD, "1. Ingresar los coeficientes de la funcion.", 0xA, 0xD, "2. Imprimir la funcion almacenada.", 0xA, 0xD, "3. Imprimir la derivada de la funcion almacenada.", 0xA, 0xD, "4. Imprimir la integral de la funcion almacenada.", 0xA, 0xD, "5. Graficar la funcion original, derivada o integral.", 0xA, 0xD, "6. Encontrar los ceros de la funcion por el metodo de Newton.", 0xA, 0xD, "7. Encontrar los ceros de la funcion por el metodo de Steffensen", 0xA, 0xD, "8. Salir de la aplicacion.", 0xA, 0xD, "Ingrese el numero de la opcion que desee realizar: "
+	;; DEFINIENDO TEXTOS DE MENÚS
+	text_menu	db	"========== CALCULADORA ARQUI 1 - Elias Vasquez ==========", 0xA, 0xD, "1. Ingresar los coeficientes de la funcion.", 0xA, 0xD, "2. Imprimir la funcion almacenada.", 0xA, 0xD, "3. Imprimir la derivada de la funcion almacenada.", 0xA, 0xD, "4. Imprimir la integral de la funcion almacenada.", 0xA, 0xD, "5. Graficar la funcion original, derivada o integral.", 0xA, 0xD, "6. Encontrar los ceros de la funcion por el metodo de Newton.", 0xA, 0xD, "7. Encontrar los ceros de la funcion por el metodo de Steffensen", 0xA, 0xD, "8. Salir de la aplicacion.", 0xA, 0xD, "> Ingrese el numero de la opcion que desee realizar: "
 	len_menu	equ	$-text_menu
 
 	text1		db	"========== CALCULADORA ARQUI 1 - Elias Vasquez ==========", 0xA, 0xD, "(1) Ingresar los coeficientes de la funcion.", 0xA, 0xD
 	len_text1	equ	$-text1
+	text1_0		db 	"> Ingrese el coeficiente de x⁰: "
+	len_text1_0	equ	$-text1_0
+	text1_1		db 	"> Ingrese el coeficiente de x¹: "
+	len_text1_1	equ	$-text1_1
+	text1_2		db 	"> Ingrese el coeficiente de x²: "
+	len_text1_2	equ	$-text1_2
+	text1_3		db 	"> Ingrese el coeficiente de x³: "
+	len_text1_3	equ	$-text1_3
+	text1_4		db 	"> Ingrese el coeficiente de x⁴: "
+	len_text1_4	equ	$-text1_4
+	text1_5		db 	"> Ingrese el coeficiente de x⁵: "
+	len_text1_5	equ	$-text1_5
+	success_1	db	0xA, 0xD, "> Coeficientes ingresados con exito.", 0xA, 0xD
+	len_succ_1	equ $-success_1
 
 	text2		db	"========== CALCULADORA ARQUI 1 - Elias Vasquez ==========", 0xA, 0xD, "(2) Imprimir la funcion almacenada.", 0xA, 0xD
 	len_text2	equ	$-text2
+	text2_0		db	"Grado de la funcion: "
+	len_text2_0	equ $-text2_0
+	text2_1		db	"f(x) = "
+	len_text2_1	equ $-text2_1
+	plus		db	" + "
+	len_plus	equ	$-plus
+	minus		db	" - "
+	len_minus	equ	$-minus
+	x_one		db	"x¹"
+	len_x_one	equ $-x_one
+	x_two		db	"x²"
+	len_x_two	equ $-x_two
+	x_three		db	"x³"
+	len_x_three	equ $-x_three
+	x_four		db	"x⁴"
+	len_x_four	equ $-x_four
+	x_five		db	"x⁵"
+	len_x_five	equ $-x_five
 
 	text3		db	"========== CALCULADORA ARQUI 1 - Elias Vasquez ==========", 0xA, 0xD, "(3) Imprimir la derivada de la funcion almacenada.", 0xA, 0xD
 	len_text3	equ	$-text3
@@ -48,23 +153,113 @@ section .data
 	error1		db	"> Error, ingrese una opcion valida. Entrada detectada: "
 	len_error1	equ	$-error1
 
+	; error de entrada inválida en opción 1
+	error2		db	"> Error, se detecto una entrada invalida. Valor a asignar al coeficiente: 0", 0xA, 0xD
+	len_error2	equ	$-error2
+
 	; salto de línea
 	ln  		db 	0xA, 0xD
+
+	;; DEFINIENDO COEFICIENTES (-128 (-2⁷) hasta 128 (2⁷))
+	degree		db	0
+	coef_0		db	0
+	coef_1		db	0
+	coef_2		db	0
+	coef_3		db	0
+	coef_4		db	0
+	coef_5		db	0
 
 
 ;; RESERVANDO ESPACIOS
 section .bss
-	buffer_in	resb 16 ; buffer de lectura
+	; Buffer de lectura
+	buffer_in	resb 16
+	; Byte para almacenar texto
+	byte_aux	resb 1
+	; Bytes para almacenar cocientes y residuos
+	cociente 	resb 1
+	residuo		resb 1
 
 
 ;; ÁREA DE CÓDIGO
 section .text
 
+;; PROCEDIMIENTO LECTURA DE NÚMERO
+;; DL: Valor de coeficiente
+;; byte_aux: Positivo o negativo
+READ_NUMBER:
+	read buffer_in, 16
+	cld
+	xor esi, esi ; limpiando el registro
+	mov esi, buffer_in
+
+	mov cl, 0 ; aux
+	mov dl, 0 ; coeficiente
+	mov [byte_aux], cl ; 0 positivo, 1 negativo
+
+	lodsb
+	; Verificando si se saltó el coeficiente
+	cmp al, 10
+	je exit_reading
+	; Verificando si se ingresó signo
+	cmp al, '-'
+	je with_minus
+	cmp al, '+'
+	je with_plus
+	; Verificando que sea número
+	cmp al, '0'
+	jb entry_readNum_error
+	cmp al, '9'
+	jbe reading
+	; Entrada inválida
+	jmp entry_readNum_error
+with_minus:
+	mov cl, 1
+	mov [byte_aux], cl ; negativo
+	lodsb
+	cmp al, '0'
+	jb entry_readNum_error
+	cmp al, '9'
+	ja entry_readNum_error
+	jmp reading
+with_plus:
+	lodsb
+	cmp al, '0'
+	jb entry_readNum_error
+	cmp al, '9'
+	ja entry_readNum_error
+reading:
+	sub al, 48 	; conviertiendo el número leído de 'num' -> num
+	mov cl, al 	; copiando el número leído a CL
+	mov al, dl 	; copiando a AL lo que se lleva en coeficiente (DL), para multiplicarlo * 10
+
+	mov bl, 10 	; Fuente: factor *10 por cada número leído
+	mul bl		; mul Fuente -> (AL * BL = AH:AL), en AL está el resultado
+
+	add al, cl 	; Sumandole a AL el número leído
+	mov dl, al 	; Guardando el coeficiente actual en DL
+
+	lodsb
+	cmp al, 10
+	je exit_reading ; si es igual, siguiente coeficiente
+	cmp al, '0'
+	jb entry_readNum_error ; si es menor, error en la entrada
+	cmp al, '9'
+	jbe reading ; si es menor o igual, entrada correcta
+entry_readNum_error:
+	print error2, len_error2
+	mov dl, 0
+	mov [byte_aux], dl
+exit_reading:
+	ret ;; Para que vuelva a la ejecución desde el punto en que se llamó
+
+
+;; INICIO DE EJECUCIÓN DE PROGRAMA
 global _start
 
 _start:
 
-menu:
+MENU:
 
 	print text_menu, len_menu
 
@@ -84,59 +279,130 @@ menu:
 
 	; SALTANDO A LA OPCIÓN QUE SE HAYA ESCOGIDO
 	cmp al, '1'
-	je option_1
+	je OPTION_1
 	cmp al, '2'
-	je option_2
+	je OPTION_2
 	cmp al, '3'
-	je option_3
+	je OPTION_3
 	cmp al, '4'
-	je option_4
+	je OPTION_4
 	cmp al, '5'
-	je option_5
+	je OPTION_5
 	cmp al, '6'
-	je option_6
+	je OPTION_6
 	cmp al, '7'
-	je option_7
+	je OPTION_7
 	cmp al, '8'
-	je exit
+	je EXIT_PROGRAM
 
 entry_error:
-	; SI ENTRA AQUÍ, SE DETECTÓ UNA ENTRADA INVÁLIDA
+	; ENTRADA INVÁLIDA
 	print error1, len_error1
 	print buffer_in, 16
 	print ln, 2
-	jmp menu
+	jmp MENU
 
-option_1:
+
+OPTION_1:
+	;; Ingresar los coeficientes de la funcion
 	print text1, len_text1
+
+	; COEFICIENTE 0
+	print text1_0, len_text1_0
 	print ln, 2
-	jmp menu
-option_2:
+read_coef_1:
+	; COEFICIENTE 1
+	print text1_1, len_text1_1
+	print ln, 2
+read_coef_2:
+	; COEFICIENTE 2
+	print text1_2, len_text1_2
+	print ln, 2
+read_coef_3:
+	; COEFICIENTE 3
+	print text1_3, len_text1_3
+	print ln, 2
+read_coef_4:
+	; COEFICIENTE 4
+	print text1_4, len_text1_4
+	print ln, 2
+read_coef_5:
+	; COEFICIENTE 5
+	print text1_5, len_text1_5
+	print ln, 2
+end_coefs:
+	print success_1, len_succ_1
+	print ln, 2
+
+	jmp MENU
+
+
+OPTION_2:
 	print text2, len_text2
 	print ln, 2
-	jmp menu
-option_3:
+	; Imprimiendo grado de función
+	print text2_0, len_text2_0
+	mov al, [degree]
+	add al, 48 ; transformando a ascii
+	mov [byte_aux], al
+	print byte_aux, 1
+	print ln, 2
+	;Imprimiendo función
+	print text2_1, len_text2_1
+	mov al, [coef_5]
+	printNumber al
+	print x_five, len_x_five
+	print plus, len_plus
+	mov al, [coef_4]
+	printNumber al
+	print x_four, len_x_four
+	print plus, len_plus
+	mov al, [coef_3]
+	printNumber al
+	print x_three, len_x_three
+	print plus, len_plus
+	mov al, [coef_2]
+	printNumber al
+	print x_two, len_x_two
+	print plus, len_plus
+	mov al, [coef_1]
+	printNumber al
+	print x_one, len_x_one
+	print plus, len_plus
+	mov al, [coef_0]
+	printNumber al
+
+	print ln, 2
+	print ln, 2
+
+	jmp MENU
+
+OPTION_3:
 	print text3, len_text3
 	print ln, 2
-	jmp menu
-option_4:
+	jmp MENU
+
+OPTION_4:
 	print text4, len_text4
 	print ln, 2
-	jmp menu
-option_5:
+	jmp MENU
+
+OPTION_5:
 	print text5, len_text5
 	print ln, 2
-	jmp menu
-option_6:
+	jmp MENU
+
+OPTION_6:
 	print text6, len_text6
 	print ln, 2
-	jmp menu
-option_7:
+	jmp MENU
+
+OPTION_7:
 	print text7, len_text7
 	print ln, 2
-	jmp menu
+	jmp MENU
 
-exit:
+EXIT_PROGRAM:
 	print text8, len_text8
 
 	mov eax, 1
